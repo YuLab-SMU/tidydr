c6024d2468d2407bd805cc257c0d42ed78b1c88d
huerqiang <13766876214@163.com>
2021-12-05 22:57:46 +0800

    update `dr_extract`

--- NEWS.md
+++ NEWS.md
@@ -1,5 +1,6 @@
-# tidydr 0.0.1
+# tidydr 0.0.1.991
 
++ update `dr_extract`: add more methods and modify the output(2021-12-5, Sun)
 + `theme_dr()` that add shorten version of x and y axis
 + `theme_noaxis()` to remove axis elements
 + `autoplot()` to plot `dr()` output
--- R/method-dr-extract.R
+++ R/method-dr-extract.R
@@ -8,13 +8,114 @@
 ##' @author Guangchuang Yu
 dr_extract <- function(result) UseMethod("dr_extract")
 
+##' @method dr_extract default
+dr_extract.default <- function(result) {
+    drdata <- eigenvalue <- stress <- NULL
+    if ("points" %in% names(result)) {
+        drdata <- as.data.frame(result$points)
+    }
+    if ("eig" %in% names(result)) {
+        eigenvalue <- as.numeric(result$eig)
+    }
+    if ("stress" %in% names(result)) {
+        stress <- result$stress
+        stress <- format(stress, digits=4)
+    }
+    list(drdata = drdata, eigenvalue = eigenvalue, stress = stress)
+}
+
+
 ##' @method dr_extract Rtsne
 dr_extract.Rtsne <- function(result) {
-    as.data.frame(result$Y)
+    ## Rtsne::Rtsne
+    drdata <- as.data.frame(result$Y)
+    eigenvalue <- NULL
+    stress <- NULL
+    list(drdata = drdata, eigenvalue = eigenvalue, stress = stress)
 }
 
 ##' @method dr_extract prcomp
 dr_extract.prcomp <- function(result) {
-    as.data.frame(result$x)
+    ## stats::prcomp
+    drdata <- as.data.frame(result$x)
+    eigenvalue <- result$sdev
+    stress <- NULL
+    list(drdata = drdata, eigenvalue = eigenvalue, stress = stress)
 }
 
+
+##' @method dr_extract MASS
+dr_extract.MASS <- function(result) {
+    ## MASS::sammon
+    drdata <- as.data.frame(result$points)
+    eigenvalue <- NULL
+    stress <- result$stress
+    stress <- format(stress, digits=4)
+    list(drdata = drdata, eigenvalue = eigenvalue, stress = stress)
+}
+
+##' @method dr_extract monoMDS
+dr_extract.monoMDS <- function(result) {
+    ## vegan::metaMDS
+    drdata <- as.data.frame(result$points)
+    eigenvalue <- NULL
+    stress <- result$stress
+    stress <- format(stress, digits=4)
+    list(drdata = drdata, eigenvalue = eigenvalue, stress = stress)
+}
+
+
+##' @method dr_extract wcmdscale
+dr_extract.wcmdscale <- function(result) {
+    ## vegan::wcmdscale
+    drdata <- as.data.frame(result$points)
+    stress <- NULL
+    eigenvalue <- as.numeric(result$eig)
+    list(drdata = drdata, eigenvalue = eigenvalue, stress = stress)
+}
+
+##' @method dr_extract ape
+dr_extract.ape <- function(result) {
+    ## ape::pcoa
+    drdata <- as.data.frame(result$vectors)
+    stress <- NULL
+    eigenvalue <- as.numeric(result$values$Eigenvalues)
+    list(drdata = drdata, eigenvalue = eigenvalue, stress = stress)
+}
+
+##' @method dr_extract smacof
+dr_extract.smacof <- function(result) {
+    ## smacof::mds
+    drdata <- as.data.frame(result$conf)
+    eigenvalue <- NULL
+    stress <- result$stress
+    stress <- format(stress, digits=4)
+    list(drdata = drdata, eigenvalue = eigenvalue, stress = stress)
+}
+
+##' @method dr_extract ecodist
+dr_extract.ecodist <- function(result) {
+    ## ecodist::pco
+    drdata <- as.data.frame(result$vectors)
+    stress <- NULL
+    eigenvalue <- as.numeric(result$values)
+    list(drdata = drdata, eigenvalue = eigenvalue, stress = stress)
+}
+
+##' @method dr_extract labdsv
+dr_extract.labdsv <- function(result) {
+    ## labdsv::pco
+    drdata <- as.data.frame(result$points)
+    stress <- NULL
+    eigenvalue <- as.numeric(result$eig)
+    list(drdata = drdata, eigenvalue = eigenvalue, stress = stress)
+}
+
+##' @method dr_extract ade4
+dr_extract.ade4 <- function(result) {
+    ## ade4::dudi.pco
+    drdata <- as.data.frame(result$tab)
+    stress <- NULL
+    eigenvalue <- as.numeric(result$eig)
+    list(drdata = drdata, eigenvalue = eigenvalue, stress = stress)
+}
