14c7b7a80eea369a65585a8274bfce461ec4a580
Guangchuang Yu <guangchuangyu@gmail.com>
2025-07-26 07:13:30 +0800

    on cran

--- R/method-dr-extract.R
+++ R/method-dr-extract.R
@@ -10,6 +10,7 @@ dr_extract <- function(result) UseMethod("dr_extract")
 
 
 ##' @method dr_extract Rtsne
+#' @export
 dr_extract.Rtsne <- function(result) {
     ## Rtsne::Rtsne
     drdata <- as.data.frame(result$Y)
@@ -19,6 +20,7 @@ dr_extract.Rtsne <- function(result) {
 }
 
 ##' @method dr_extract prcomp
+#' @export
 dr_extract.prcomp <- function(result) {
     ## stats::prcomp
     drdata <- as.data.frame(result$x)
@@ -29,6 +31,7 @@ dr_extract.prcomp <- function(result) {
 
 
 ##' @method dr_extract ape
+#' @export
 dr_extract.ape <- function(result) {
     ## ape::pcoa
     drdata <- as.data.frame(result$vectors)
@@ -38,6 +41,7 @@ dr_extract.ape <- function(result) {
 }
 
 ##' @method dr_extract smacof
+#' @export
 dr_extract.smacof <- function(result) {
     ## smacof::mds
     drdata <- as.data.frame(result$conf)
@@ -48,6 +52,7 @@ dr_extract.smacof <- function(result) {
 }
 
 ##' @method dr_extract ecodist
+#' @export
 dr_extract.ecodist <- function(result) {
     ## ecodist::pco
     drdata <- as.data.frame(result$vectors)
@@ -58,6 +63,7 @@ dr_extract.ecodist <- function(result) {
 
 
 ##' @method dr_extract ade4
+#' @export
 dr_extract.ade4 <- function(result) {
     ## ade4::dudi.pco
     drdata <- as.data.frame(result$tab)
@@ -67,7 +73,8 @@ dr_extract.ade4 <- function(result) {
 }
 
 ##' @method dr_extract uwot
-##' @author Lang Zhou
+## @author Lang Zhou
+#' @export
 dr_extract.uwot <- function(result) {
     ## uwot::umap
     ## uwot::tumap
@@ -78,6 +85,7 @@ dr_extract.uwot <- function(result) {
 }
 
 ##' @method dr_extract default
+#' @export
 dr_extract.default <- function(result) {
     drdata <- eigenvalue <- stress <- NULL
     if ("points" %in% names(result)) {
@@ -96,18 +104,22 @@ dr_extract.default <- function(result) {
 ## vegan::metaMDS
 ## no eigenvalue
 ##' @method dr_extract monoMDS
+#' @export
 dr_extract.monoMDS <- dr_extract.default
     
 ## MASS::sammon
 ##' @method dr_extract MASS
+#' @export
 dr_extract.MASS <- dr_extract.monoMDS
 
 ## vegan::wcmdscale
 ## no stress
 ##' @method dr_extract wcmdscale
+#' @export
 dr_extract.wcmdscale <- dr_extract.default
     
 ## labdsv::pco
 ##' @method dr_extract labdsv
+#' @export
 dr_extract.labdsv <- dr_extract.wcmdscale
 
